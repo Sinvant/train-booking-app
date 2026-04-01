267f09562f26fdb47d87987f4c252a995e106ac3
Sinvant <singhaniketprasun@gmail.com>
2026-04-01 16:19:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/TRS";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://Sinvant:qwerty1234@cluster0.bswwad4.mongodb.net/?appName=Cluster0";
 
 app.use(cors());
 app.use(express.json());
